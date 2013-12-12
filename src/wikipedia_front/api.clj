493b4e6564c114949edd58f8ab0d0cf53668c8f5
Chris Perivolaropoulos <darksaga2006@gmail.com>
2013-12-12 03:21:41 +0200

    Hiding a bit of complexity of overriding defns

--- src/wikipedia_front/api.clj
+++ src/wikipedia_front/api.clj
@@ -3,14 +3,18 @@
 
 (def wb-functions (atom {}))
 
+(defmacro defn-sym
+  "Create a function and get the symbol."
+  [name fdecls]
+  `(eval (macroexpand (list* `defn ~name ~fdecls))))
+
 (defmacro wb-defn
   "Defines a function and informs wb-functions about it. You may use
   ^{:cmd-name <name>} to define the name of the function in the
   interface. Use this to avoid namespace conflicts."
   [name & fdecls]
-
-  (let [fsym (eval (macroexpand (list* `defn name fdecls)))]
+  (let [fsym (defn-sym name fdecls)]
     (reset! wikipedia-front.api/wb-functions
             (assoc @wikipedia-front.api/wb-functions
               (or (:cmd-name (meta name)) (str name)) fsym))
-  fsym))
+    fsym))
